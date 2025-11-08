4f782b26ed43fafd66e4eb678cc43799a1bb8823
Vitoria das Dores <vitoria.dores@ufrpe.br>
2025-11-08 01:08:45 -0300

    ADD MongoDB connection setup and update package dependencies

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import router from './routes/index.js'
+import connectDB from '../Config/db.js';
 
 const app = express()
 
 app.use(router);
+
+// Conect Database
+connectDB();
 
 app.listen(3000, () => {
     console.log('server running at  http://st:3000')
