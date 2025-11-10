e9b0c97f521b4bcf2e4b4e3682cbb6346c9c5e5b
Letícia Staudinger <lsr@cin.ufpe.br>
2025-11-10 12:32:00 -0300

    Ajuste em Local.js para termos somente uma descrição por local

--- server/src/models/Local.js
+++ server/src/models/Local.js
@@ -1,26 +1,30 @@
 import mongoose from "mongoose";
+
+// Sub-schema para a descrição do local
+const DescricaoSchema = new mongoose.Schema({
+    // Descrição única por local, com um único autor (por enquanto)
+    texto: { type: String, required: true },
+    autor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }, // Autor da descrição
+    curtidas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Usuario" }], // Array com id's de quem curtiu
+    criado_em: { type: Date, default: Date.now }
+});
+
 const LocalSchema = new mongoose.Schema({
-    nome: {type: String, required: True},
-    descricao: {
-        id: {type: mongoose.hema.Types.ObjectId, auto: True}, //Id automático para cada descrição
-        texto: {type: String, required: True},
-        curtidas: [{type: mongoose.Schema.Types.ObjectId, ref: "Usuario"}] //Array com id's de quem curtiu
-    },
-    imagem_url: {type: String, required: True},
-    autor_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: True},
+    nome: { type: String, required: true }, // Nome do local
+    descricao: DescricaoSchema, // Objeto (embedado) que segue o DescricaoSchema, já que só terá uma descrição por local
+    imagem_url: { type: String, required: true },
+    autor_id_cadastro: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }, // Quem cadastrou o local pela primeira vez
     endereco: {
         coordenadas: {
-            latitude: {type: Number, required: True},
-            longitude: {type: Number, required: True}
+            latitude: { type: Number, required: true },
+            longitude: { type: Number, required: true }
         }
     },
-    criado_em: {type: Date, default: Date.now}
+    criado_em: { type: Date, default: Date.now }
+});
 
-})
-
-// Indice único por coordenada
-LocalSchema.index({"endereco.coordenadas.latitude": 1, "endereco.coordenadas.latitude": 1}, {unique: True});
-
+// Indice único por coordenada (corrige o campo duplicado)
+LocalSchema.index({ "endereco.coordenadas.latitude": 1, "endereco.coordenadas.longitude": 1 }, { unique: true });
 
 const Local = mongoose.model("Local", LocalSchema);
 
