40ea052bd1b15536f7e54c9bf31537942bd93bd1
Ana Laura <ana.lauraboliveiras@gmail.com>
2025-11-10 23:01:54 -0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './App/Home';
 import TestPage from './App/TestPage';
-
-import PaginaInicial from './Home/PaginaInicial';
-
-<Route path="/" element={<PaginaInicial />} />
-
+import PaginaInicial from './Home/PaginaInicial'; 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* Página inicial */}
+      <Route path="/" element={<PaginaInicial />} />
+      
+      {/* Outras rotas */}
+      <Route path="/home" element={<Home />} />
       <Route path="/test" element={<TestPage />} />
     </Routes>
   );
 }
 
 export default App;
+
