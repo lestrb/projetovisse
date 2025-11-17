28326c2c48287f2ddcf8b7122ff6048ddb23f2c8
safiracode <safira.moraes.dev@gmail.com>
2025-11-17 18:55:00 -0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema ({
-    nome : {type: String, required: true},
-    email : {type: String, required: true},
+    nome : {type: String, required: true, trim: true},
+    email : {type: String, required: true, lowercase: true, unique: true, trim: true},
     senha : {type: String, required: true},
-    data : {}
+    data : {type: Date, default: Date.now}
 })
 
 export default mongoose.model("User", UserSchema);
