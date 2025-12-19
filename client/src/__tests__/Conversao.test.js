067e28d1669dbc5519e2929be839df2a2c02fbc9
Edo-Alve5 <duduapvo@gmail.com>
2025-12-19 15:44:47 -0300

    Correção teste de front

--- client/src/__tests__/Conversao.test.js
+++ client/src/__tests__/Conversao.test.js
@@ -1,13 +1,9 @@
+import '@testing-library/jest-dom';
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ConversaoCapibaScreen from '../pages/ConversaoCapiba/ConversaoCapibaScreen';
 import { BrowserRouter } from 'react-router-dom';
-
-const mockedNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockedNavigate,
-}));
 
 describe('BB-04: Teste de Interface - Conversão', () => {
   test('Deve exibir o texto de moedas na tela', () => {
